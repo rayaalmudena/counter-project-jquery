ff09f31fee0d7f45d13930eb89ced146d008d64d
rayaalmudena <raya.almudena@gmail.com>
2022-03-28 09:28:57 +0200

    28.03.2022. 9:28 done

--- js/app.js
+++ js/app.js
@@ -3,4 +3,24 @@ let counter = 0;
 $(document).ready(function () {
     console.log("DOM cargado! Implementa tu solución a partir de esta línea.");
 
+
+    $(".prevBtn").click(function () {
+        $("#counter").text(+($("#counter").text()) - 1);
+        changeColor();
+    });
+
+    $(".nextBtn").click(function () {
+        $("#counter").text(+($("#counter").text()) + 1);
+        changeColor();
+    });
+
+    function changeColor(params) {
+        if(+$("#counter").text() > 0) {
+            $("#counter").css("color", "green");
+        } else if(+$("#counter").text() < 0) {
+            $("#counter").css("color", "green");
+        } else {
+            $("#counter").css("color", "gray");
+        }
+    }
 });
